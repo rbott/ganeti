d24c616c43749525fcd8c041ed9c401fa819e529
Rudolph Bott <r@spam.wtf>
2021-11-16 20:04:55 +0100

    fix typo

--- lib/hypervisor/hv_docker.py
+++ lib/hypervisor/hv_docker.py
@@ -132,13 +132,15 @@ class DockerHypervisor(hv_base.BaseHypervisor):
 
     """
     hvp = instance.hvparams
+    logging.info(hvp)
     logging.info("Pulling image '%s:%s'" %(hvp[constants.HV_DOCKER_IMAGE],
                                            hvp[constants.HV_DOCKER_TAG]))
     self.docker.images.pull("%s:%s" % (hvp[constants.HV_DOCKER_IMAGE],
                                        hvp[constants.HV_DOCKER_TAG]))
     self.docker.containers.run("%s:%s" % (hvp[constants.HV_DOCKER_IMAGE],
                                           hvp[constants.HV_DOCKER_TAG]),
-                               detach=True, name=instance.name, remove=True)
+                               detach=True, name=instance.name, remove=True,
+                               mem_limit=134217728)
 
 
   def StopInstance(self, instance, force=False, retry=False, name=None,
