1228285eee6d304e396a3a7f50a441edb5261dec
Rudolph Bott <r@spam.wtf>
2021-11-16 20:16:20 +0100

    fix typo

--- lib/hypervisor/hv_docker.py
+++ lib/hypervisor/hv_docker.py
@@ -99,8 +99,7 @@ class DockerHypervisor(hv_base.BaseHypervisor):
       logging.info("yep, it is")
       id = container.attrs['Id']
       state = hv_base.HvInstanceState.RUNNING
-      memory = container.attrs['Hostconfig']['Memory']
-      logging.info("RUNNING MEMORY: %d" % memory)
+      memory = container.attrs['HostConfig']['Memory']
       vcpus = 1
     else:
       logging.info("no, its not")
