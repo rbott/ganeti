cd1203622c53ffb9261ff9dd06bfa9c1bfd71725
Rudolph Bott <r@spam.wtf>
2021-11-17 20:55:56 +0100

    fix typo

--- lib/hypervisor/hv_docker.py
+++ lib/hypervisor/hv_docker.py
@@ -135,11 +135,10 @@ class DockerHypervisor(hv_base.BaseHypervisor):
     cpu_period = 100000
     cpu_quota = be[constants.BE_VCPUS] * 100000
     
-    logging.debug(block_devices)
     volumes = []
     for bdev in block_devices:
       path = os.readlink(bdev[1])
-      mountpoint = "/root"
+      mountpoint = bdev[0]["name"]
       volumes.append(Mount(mountpoint, None, type='volume',
                            read_only=False,
                            driver_config=DriverConfig(
