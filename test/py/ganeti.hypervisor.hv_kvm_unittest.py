184b5034c930a72784f9bc9c8a664108184c7a4a
Rudolph Bott <r@spam.wtf>
2022-04-06 20:13:56 +0200

    KVM: replace -drive by -blockdev and make HMP obsolete
    This commit drops support for the deprecated -drive parameter of QEMU
    (in favor of -blockdev). It also switches disk hotplugging to use
    blockdevadd/blockdev-del via QMP which was the last part of Ganeti which
    made use of QEMU's human monitor (HMP).
    
    Along some adjustments are made to KVM parameters:
    * disk_aio now also supports io_uring on QEMU 5.0+ (default: threads)
    * disk_discard now only accepts ignore/unmap (default: ignore)
    
    Signed-off-by: Rudolph Bott <r@spam.wtf>

--- test/py/ganeti.hypervisor.hv_kvm_unittest.py
+++ test/py/ganeti.hypervisor.hv_kvm_unittest.py
@@ -377,22 +377,6 @@ class TestParameterCheck(testutils.GanetiTestCase):
     self.assertRaises(errors.HypervisorError,
                       validation.check_disk_cache_parameters, invalid_data)
 
-    invalid_data = {
-      constants.HV_KVM_DISK_AIO: constants.HT_KVM_AIO_NATIVE,
-      constants.HV_DISK_CACHE: constants.HT_CACHE_WTHROUGH
-    }
-
-    self.assertRaises(errors.HypervisorError,
-                      validation.check_disk_cache_parameters, invalid_data)
-
-    invalid_data = {
-      constants.HV_KVM_DISK_AIO: constants.HT_KVM_AIO_NATIVE,
-      constants.HV_DISK_CACHE: constants.HT_CACHE_DEFAULT
-    }
-
-    self.assertRaises(errors.HypervisorError,
-                      validation.check_disk_cache_parameters, invalid_data)
-
   def testValidDiskCacheParameters(self):
     valid_data = {
       constants.HV_KVM_DISK_AIO: constants.HT_KVM_AIO_THREADS,
@@ -403,14 +387,14 @@ class TestParameterCheck(testutils.GanetiTestCase):
 
     valid_data = {
       constants.HV_KVM_DISK_AIO: constants.HT_KVM_AIO_THREADS,
-      constants.HV_DISK_CACHE: constants.HT_CACHE_WTHROUGH
+      constants.HV_DISK_CACHE: constants.HT_CACHE_DEFAULT
     }
 
     self.assertTrue(validation.check_disk_cache_parameters(valid_data))
 
     valid_data = {
       constants.HV_KVM_DISK_AIO: constants.HT_KVM_AIO_THREADS,
-      constants.HV_DISK_CACHE: constants.HT_CACHE_DEFAULT
+      constants.HV_DISK_CACHE: constants.HT_CACHE_WTHROUGH
     }
 
     self.assertTrue(validation.check_disk_cache_parameters(valid_data))
@@ -557,59 +541,6 @@ class TestParameterValidation(testutils.GanetiTestCase):
 
     self.assertTrue(validation.validate_spice_parameters(valid_data,
                                                          kvm_help_working))
-
-
-class TestDiskParameters(testutils.GanetiTestCase):
-  def testGenerateDiskAioCacheParameters(self):
-    test_cases = {
-      "aio_threaded_safe_storage_default_cache": {
-        "disk_aio": constants.HT_KVM_AIO_THREADS,
-        "disk_cache": constants.HT_CACHE_DEFAULT,
-        "dev_type": constants.DT_DRBD8,
-        "expected_string": ",aio=threads"
-      },
-      "aio_threaded_unsafe_storage_default_cache": {
-        "disk_aio": constants.HT_KVM_AIO_THREADS,
-        "disk_cache": constants.HT_CACHE_DEFAULT,
-        "dev_type": constants.DT_SHARED_FILE,
-        "expected_string": ",aio=threads,cache=none"
-      },
-      "aio_threaded_safe_storage_writeback_cache": {
-        "disk_aio": constants.HT_KVM_AIO_THREADS,
-        "disk_cache": constants.HT_CACHE_WBACK,
-        "dev_type": constants.DT_RBD,
-        "expected_string": ",aio=threads,cache=writeback"
-      },
-      "aio_native_safe_storage_none_cache": {
-        "disk_aio": constants.HT_KVM_AIO_NATIVE,
-        "disk_cache": constants.HT_CACHE_NONE,
-        "dev_type": constants.DT_DRBD8,
-        "expected_string": ",aio=native,cache=none"
-      },
-      "aio_native_safe_storage_writethrough_cache": {
-        "disk_aio": constants.HT_KVM_AIO_NATIVE,
-        "disk_cache": constants.HT_CACHE_WTHROUGH,
-        "dev_type": constants.DT_DRBD8,
-        "expected_string": ",aio=native,cache=none"
-      },
-      "aio_native_unsafe_storage_writethrough_cache": {
-        "disk_aio": constants.HT_KVM_AIO_NATIVE,
-        "disk_cache": constants.HT_CACHE_WTHROUGH,
-        "dev_type": constants.DT_GLUSTER,
-        "expected_string": ",aio=native,cache=none"
-      },
-      "aio_unset_safe_storage_none_cache": {
-        "disk_aio": None,
-        "disk_cache": constants.HT_CACHE_NONE,
-        "dev_type": constants.DT_DRBD8,
-        "expected_string": ",aio=threads,cache=none"
-      }
-    }
-
-    for name, data in test_cases.items():
-      self.assertEqual(hv_kvm.KVMHypervisor._GenerateDiskAioCacheParameters(
-        data["disk_aio"], data["disk_cache"], data["dev_type"]),
-        data["expected_string"], name)
 
 
 class TestQmpMessage(testutils.GanetiTestCase):
@@ -1050,7 +981,6 @@ class TestKvmRuntime(testutils.GanetiTestCase):
     self.MockOut('pid_alive', mock.patch(kvm_class + '._InstancePidAlive',
                                          return_value=('file', -1, False)))
     self.MockOut(mock.patch(kvm_class + '._ExecuteCpuAffinity'))
-    self.MockOut(mock.patch(kvm_class + '._CallMonitorCommand'))
 
     self.cfg = ConfigMock()
     params = constants.HVC_DEFAULTS[constants.HT_KVM].copy()
