f7022c9ffdaf18e3a7de3ac18e1c146a02d518e7
Rudolph Bott <r@bott.im>
2025-02-11 19:43:51 +0100

    Revert new cluster default for machine_version hvparam
    This commit partly reverts 86a41cecb24487f78ae867cefb0cadcd80c0df63
    
    The new default value `pc` for `machine_version` will cause more
    trouble than initially assumed. It might break instances during
    live-migration if the receiving end is a newer kvm release (e.g.
    during rolling cluster upgrades), since that end has a different
    understanding of what `pc` actually is. It would be more sane to
    configure an exact version, like `pc-i440fx-9.2`.
    
    Documentation has been updated and also the cluster parameter
    assessment now warns if `machine_version` is set to unsupported
    values or to values that might cause trouble.
    
    Signed-off-by: Rudolph Bott <r@bott.im>

--- src/Ganeti/Constants.hs
+++ src/Ganeti/Constants.hs
@@ -2992,11 +2992,6 @@ htMigrationModes :: FrozenSet String
 htMigrationModes =
   ConstantUtils.mkSet $ map Types.migrationModeToRaw [minBound..]
 
--- * Default Machine Type
-
-htKvmMachineVersionPc :: String
-htKvmMachineVersionPc = "pc"
-
 -- * Cluster verify steps
 
 verifyNplusoneMem :: String
@@ -4174,7 +4169,7 @@ hvcDefaults =
           , (hvUsbDevices,                      PyValueEx "")
           , (hvVga,                             PyValueEx "")
           , (hvKvmExtra,                        PyValueEx "")
-          , (hvKvmMachineVersion,               PyValueEx htKvmMachineVersionPc)
+          , (hvKvmMachineVersion,               PyValueEx "")
           , (hvKvmMigrationCaps,                PyValueEx "")
           , (hvVnetHdr,                         PyValueEx True)])
   , (KvmMicro, Map.fromList
