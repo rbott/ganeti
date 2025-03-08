aa33b46b550e75ba7fc38477eb38ed7a9226c856
Rudolph Bott <r@bott.im>
2025-03-08 23:16:49 +0100

    Extend HV kvm-micro

--- lib/hypervisor/hv_kvmmicro.py
+++ lib/hypervisor/hv_kvmmicro.py
@@ -43,6 +43,8 @@ from ganeti import objects
 from ganeti import utils
 from ganeti import pathutils
 from ganeti.hypervisor import hv_base
+from ganeti.hypervisor.hv_kvm import netdev
+from ganeti.utils import wrapper as utils_wrapper
 
 class KVMMicroHypervisor(hv_base.BaseHypervisor):
   """microvm implementation for Ganeti
@@ -99,14 +101,14 @@ class KVMMicroHypervisor(hv_base.BaseHypervisor):
     """
     alive = utils.IsProcessAlive(pid)
     if not alive:
-      raise errors.HypervisorError("Cannot get info for pid %s" % pid)
+      raise errors.HypervisorError(f"Cannot get info for pid {pid}")
 
     cmdline_file = utils.PathJoin("/proc", str(pid), "cmdline")
     try:
       cmdline = utils.ReadFile(cmdline_file)
     except EnvironmentError as err:
-      raise errors.HypervisorError("Can't open cmdline file for pid %s: %s" %
-                                   (pid, err))
+      raise errors.HypervisorError(f"Can't open cmdline file for pid "
+                                   f"{pid}: {err}")
 
     instance = None
     memory = 0
@@ -123,8 +125,8 @@ class KVMMicroHypervisor(hv_base.BaseHypervisor):
         vcpus = int(arg_list.pop(0).split(",")[0])
 
     if instance is None:
-      raise errors.HypervisorError("Pid %s doesn't contain a ganeti kvm"
-                                   " instance" % pid)
+      raise errors.HypervisorError(f"Pid {pid} doesn't contain a ganeti kvm"
+                                   " instance")
 
     return (instance, memory, vcpus)
 
@@ -150,6 +152,130 @@ class KVMMicroHypervisor(hv_base.BaseHypervisor):
 
     return (pidfile, pid, alive)
 
+  @classmethod
+  def _ParseKVMVersion(cls, text):
+    """Parse the KVM version from the --help output.
+
+    @type text: string
+    @param text: output of kvm --help
+    @return: (version, v_maj, v_min, v_rev)
+    @raise errors.HypervisorError: when the KVM version cannot be retrieved
+
+    """
+    match = cls._VERSION_RE.search(text.splitlines()[0])
+    if not match:
+      raise errors.HypervisorError("Unable to get KVM version")
+
+    v_all = match.group(0)
+    v_maj = int(match.group(1))
+    v_min = int(match.group(2))
+    if match.group(4):
+      v_rev = int(match.group(4))
+    else:
+      v_rev = 0
+    return (v_all, v_maj, v_min, v_rev)
+
+  @classmethod
+  def _GetKVMOutput(cls, kvm_path, option):
+    """Return the output of a kvm invocation
+
+    @type kvm_path: string
+    @param kvm_path: path to the kvm executable
+    @type option: a key of _KVMOPTS_CMDS
+    @param option: kvm option to fetch the output from
+    @return: output a supported kvm invocation
+    @raise errors.HypervisorError: when the KVM help output cannot be retrieved
+
+    """
+    assert option in cls._KVMOPTS_CMDS, "Invalid output option"
+
+    optlist, can_fail = cls._KVMOPTS_CMDS[option]
+
+    result = utils.RunCmd([kvm_path] + optlist)
+    if result.failed and not can_fail:
+      raise errors.HypervisorError(f"Unable to get KVM {' '.join(optlist)} "
+                                   "output")
+    return result.output
+
+  @classmethod
+  def _GetKVMVersion(cls, kvm_path):
+    """Return the installed KVM version.
+
+    @return: (version, v_maj, v_min, v_rev)
+    @raise errors.HypervisorError: when the KVM version cannot be retrieved
+
+    """
+    return cls._ParseKVMVersion(cls._GetKVMOutput(kvm_path, cls._KVMOPT_HELP))
+
+  @classmethod
+  def _InstanceMonitor(cls, instance_name):
+    """Returns the instance monitor socket name
+
+    """
+    return utils.PathJoin(cls._CTRL_DIR, f"{instance_name}.monitor")
+
+  @staticmethod
+  def _SocatUnixConsoleParams():
+    """Returns the correct parameters for socat
+
+    If we have a new-enough socat we can use raw mode with an escape character.
+
+    """
+    if constants.SOCAT_USE_ESCAPE:
+      return f"raw,echo=0,escape={constants.SOCAT_ESCAPE_CODE}"
+    else:
+      return "echo=0,icanon=0"
+
+  @classmethod
+  def _InstanceSerial(cls, instance_name):
+    """Returns the instance serial socket name
+
+    """
+    return utils.PathJoin(cls._CTRL_DIR, f"{instance_name}.serial")
+
+  @classmethod
+  def _InstanceQmpMonitor(cls, instance_name):
+    """Returns the instance serial QMP socket name
+
+    """
+    return utils.PathJoin(cls._CTRL_DIR, f"{instance_name}.qmp")
+
+  @staticmethod
+  def _ConfigureNIC(instance, seq, nic, tap):
+    """Run the network configuration script for a specified NIC
+
+    See L{hv_base.ConfigureNIC}.
+
+    @param instance: instance we're acting on
+    @type instance: instance object
+    @param seq: nic sequence number
+    @type seq: int
+    @param nic: nic we're acting on
+    @type nic: nic object
+    @param tap: the host's tap interface this NIC corresponds to
+    @type tap: str
+
+    """
+    hv_base.ConfigureNIC([pathutils.KVM_IFUP, tap], instance, seq, nic, tap)
+
+  @staticmethod
+  def _GenerateDeviceKVMId(dev_type, uuid):
+    """Helper function to generate a unique device name used by KVM
+
+    QEMU monitor commands use names to identify devices. Since the UUID
+    is too long for a device ID (36 chars vs. 30), we choose to use
+    only the part until the third '-' with a disk/nic prefix.
+    For example if a disk has UUID '932df160-7a22-4067-a566-7e0ca8386133'
+    the resulting device ID would be 'disk-932df160-7a22-4067'.
+
+    @type dev_type: string
+    @param dev_type: device type of param dev (HOTPLUG_TARGET_DISK|NIC)
+    @type dev: L{objects.Disk} or L{objects.NIC}
+    @param dev: the device object for which we generate a kvm name
+
+    """
+    return f"{dev_type.lower()}-{uuid.rsplit('-', 2)[0]}"
+
   def __init__(self):
     hv_base.BaseHypervisor.__init__(self)
     # Let's make sure the directories we need exist, even if the RUN_DIR lives
@@ -174,58 +300,6 @@ class KVMMicroHypervisor(hv_base.BaseHypervisor):
     kvmpath = hvp[constants.HV_KVM_MICRO_PATH]
     pidfile = self._InstancePidFile(instance.name)
 
-    """
-    -device virtio-blk-pci,id=disk-07883d63-d0cb-41cb,bus=pci.0,addr=0xc,drive=disk-07883d63-d0cb-41cb,write-cache=on
-    -blockdev driver=raw,node-name=disk-07883d63-d0cb-41cb,discard=ignore,cache.direct=on,cache.no-flush=off,file.driver=host_device,file.filename=/var/run/ganeti/instance-disks/kvm-test-instance02.staging.ganeti.org:0,file.aio=threads,auto-read-only=off
-    """
-
-    """
-    -t file
-    disks = [
-      (
-        {
-          'dev_type': 'file',
-          'logical_id': ('loop', '/ganeti-disks/kvm-test-instance2/c6960994-b876-4ee2-b4b5-0b2ba1d213c4.file.disk0'),
-          'children': [],
-          'nodes': ['d253a43f-f52e-463e-b538-3b13d4287a77'],
-          'iv_name': 'disk/0',
-          'size': 2560,
-          'mode': 'rw',
-          'params': {},
-          'serial_no': 1,
-          'uuid': '2ea16b4a-a2d6-48ee-aaf0-31a3e4a64d29',
-          'ctime': 1738104030.8579557,
-          'mtime': 1738104030.8579545
-        },
-        '/var/run/ganeti/instance-disks/kvm-test-instance2:0',
-        None)
-    ]
-    """
-
-    """
-    -t plain
-    disks = [
-      (
-        {
-          'dev_type': 'plain',
-          'logical_id': ('gnt', '4006bd9f-3e81-4e09-97c9-caa97b3c8dfc.disk0'),
-          'children': [],
-          'nodes': ['d253a43f-f52e-463e-b538-3b13d4287a77'],
-          'iv_name': 'disk/0',
-          'size': 2560,
-          'mode': 'rw',
-          'params': {'stripes': 1},
-          'serial_no': 1,
-          'uuid': 'dd4c431c-6474-4506-9840-39a76af6e6a6',
-          'ctime': 1738104117.3526156,
-          'mtime': 1738104117.3526125
-        },
-        '/var/run/ganeti/instance-disks/kvm-test-instance2:0',
-        None
-      )
-    ]
-    """
-
     kvm_cmd = []
     kvm_cmd.append(kvmpath)
     kvm_cmd.extend(["-name", instance.name])
@@ -245,9 +319,10 @@ class KVMMicroHypervisor(hv_base.BaseHypervisor):
     kvm_cmd.extend(["-m", instance.beparams[constants.BE_MAXMEM]])
     kvm_cmd.extend(["-smp", instance.beparams[constants.BE_VCPUS]])
 
-    serial_dev = ("socket,path=%s,server=on,wait=off,id=char0" %
-                  self._InstanceSerial(instance.name))
-    if hvp[constants.HV_KVM_MICRO_SERIAL_TYPE] == constants.HT_KVM_MICRO_SERIAL_VIRT_CONSOLE:
+    serial_dev = (f"socket,path={self._InstanceSerial(instance.name)},"
+                  "server=on,wait=off,id=char0")
+    if hvp[constants.HV_KVM_MICRO_SERIAL_TYPE] == \
+      constants.HT_KVM_MICRO_SERIAL_VIRT_CONSOLE:
       machine_opt_list.append("isa-serial=off")
       kvm_cmd.extend(["-device", "virtio-serial-device",
                       "-device", "virtconsole,chardev=char0,name=console0",
@@ -266,17 +341,70 @@ class KVMMicroHypervisor(hv_base.BaseHypervisor):
     if hvp[constants.HV_KERNEL_ARGS]:
       append_params += " " + hvp[constants.HV_KERNEL_ARGS]
     kvm_cmd.extend(["-append", append_params])
+    
+    tapfds = []
+    taps = []
+    for nic_seq, nic in enumerate(instance.nics):
+      nic_id = self._GenerateDeviceKVMId("net", nic.uuid)
+      tapname, nic_tapfds, nic_vhostfds = \
+          netdev.OpenTap(features=None, name="")
+      tapfds.extend(nic_tapfds)
+      tapfds.extend(nic_vhostfds)
+      taps.append(tapname)
+      tapfd = "%s%s" % ("fds=" if len(nic_tapfds) > 1 else "fd=",
+                          ":".join(str(fd) for fd in nic_tapfds))
 
-    kvm_cmd.extend(["-daemonize", "-nodefaults", "-no-user-config", "-nographic"])
-    qemu_logfile = utils.PathJoin(pathutils.LOG_KVM_DIR,
-                                  "%s.log" % instance.name)
+      if nic_vhostfds:
+        vhostfd = "%s%s" % (",vhostfds="
+                            if len(nic_vhostfds) > 1 else ",vhostfd=",
+                            ":".join(str(fd) for fd in nic_vhostfds))
+      else:
+        vhostfd = ""
+      
+      nic_val = (f"virtio-net-device,netdev={nic_id},mac={nic.mac}")
+      tap_val = (f"type=tap,id={nic_id},{tapfd}{vhostfd}")
+      kvm_cmd.extend(["-netdev", tap_val, "-device", nic_val])
+      
+      if nic.nicparams[constants.NIC_MODE] == constants.NIC_MODE_ROUTED:
+        continue
+      self._ConfigureNIC(instance, nic_seq, nic, taps[nic_seq])
+    
+    for _, disk in enumerate(block_devices):
+      params, path, _ = disk
+      
+      disk_id = self._GenerateDeviceKVMId("disk", params.uuid)
+      if params.dev_type in constants.DTS_FILEBASED:
+        driver = "file"
+      else:
+        driver = "host_device"
+        
+      dev_val  = f"virtio-blk-device,id={disk_id},drive={disk_id}"
+      bdev_val = f"driver=raw,node-name={disk_id},file.driver={driver},"\
+                 f"file.filename={path}"
+      
+      kvm_cmd.extend(["-blockdev", bdev_val, "-device", dev_val])
+      
+
+
+    kvm_cmd.extend(["-daemonize", "-nodefaults", "-no-user-config",
+                    "-nographic"])
+    qemu_logfile = utils.PathJoin(pathutils.LOG_KVM_DIR,f"{instance.name}.log")
     kvm_cmd.extend(["-D", qemu_logfile])
+    
+    kvm_cmd.extend(["-qmp", f"unix:{self._InstanceQmpMonitor(instance.name)}"
+                    ",server,nowait"])
 
-    result = utils.RunCmd(kvm_cmd)
+    try:
+      result = utils.RunCmd(kvm_cmd, noclose_fds=tapfds)
+    finally:
+      for fd in tapfds:
+        utils_wrapper.CloseFdNoError(fd)
+
+
 
     if result.failed:
-      raise errors.HypervisorError("Failed to start instance %s: %s (%s)" %
-                                   (instance.name, result.fail_reason, result.output))
+      raise errors.HypervisorError(f"Failed to start instance {instance.name}:"
+                                   f" {result.fail_reason} ({result.output})")
 
   def StopInstance(self, instance, force=False, retry=False, name=None,
                    timeout=None):
@@ -359,61 +487,6 @@ class KVMMicroHypervisor(hv_base.BaseHypervisor):
         data.append(info)
     return data
 
-  @classmethod
-  def _ParseKVMVersion(cls, text):
-    """Parse the KVM version from the --help output.
-
-    @type text: string
-    @param text: output of kvm --help
-    @return: (version, v_maj, v_min, v_rev)
-    @raise errors.HypervisorError: when the KVM version cannot be retrieved
-
-    """
-    match = cls._VERSION_RE.search(text.splitlines()[0])
-    if not match:
-      raise errors.HypervisorError("Unable to get KVM version")
-
-    v_all = match.group(0)
-    v_maj = int(match.group(1))
-    v_min = int(match.group(2))
-    if match.group(4):
-      v_rev = int(match.group(4))
-    else:
-      v_rev = 0
-    return (v_all, v_maj, v_min, v_rev)
-
-  @classmethod
-  def _GetKVMOutput(cls, kvm_path, option):
-    """Return the output of a kvm invocation
-
-    @type kvm_path: string
-    @param kvm_path: path to the kvm executable
-    @type option: a key of _KVMOPTS_CMDS
-    @param option: kvm option to fetch the output from
-    @return: output a supported kvm invocation
-    @raise errors.HypervisorError: when the KVM help output cannot be retrieved
-
-    """
-    assert option in cls._KVMOPTS_CMDS, "Invalid output option"
-
-    optlist, can_fail = cls._KVMOPTS_CMDS[option]
-
-    result = utils.RunCmd([kvm_path] + optlist)
-    if result.failed and not can_fail:
-      raise errors.HypervisorError("Unable to get KVM %s output" %
-                                   " ".join(optlist))
-    return result.output
-
-  @classmethod
-  def _GetKVMVersion(cls, kvm_path):
-    """Return the installed KVM version.
-
-    @return: (version, v_maj, v_min, v_rev)
-    @raise errors.HypervisorError: when the KVM version cannot be retrieved
-
-    """
-    return cls._ParseKVMVersion(cls._GetKVMOutput(kvm_path, cls._KVMOPT_HELP))
-
   def GetNodeInfo(self, hvparams=None):
     """Return information about the node.
 
@@ -439,32 +512,6 @@ class KVMMicroHypervisor(hv_base.BaseHypervisor):
     return result
 
   @classmethod
-  def _InstanceMonitor(cls, instance_name):
-    """Returns the instance monitor socket name
-
-    """
-    return utils.PathJoin(cls._CTRL_DIR, "%s.monitor" % instance_name)
-
-  @staticmethod
-  def _SocatUnixConsoleParams():
-    """Returns the correct parameters for socat
-
-    If we have a new-enough socat we can use raw mode with an escape character.
-
-    """
-    if constants.SOCAT_USE_ESCAPE:
-      return "raw,echo=0,escape=%s" % constants.SOCAT_ESCAPE_CODE
-    else:
-      return "echo=0,icanon=0"
-
-  @classmethod
-  def _InstanceSerial(cls, instance_name):
-    """Returns the instance serial socket name
-
-    """
-    return utils.PathJoin(cls._CTRL_DIR, "%s.serial" % instance_name)
-
-  @classmethod
   def GetInstanceConsole(cls, instance, primary_node, node_group,
                          hvparams, beparams):
     """Return information for connecting to the console of an instance.
@@ -473,8 +520,8 @@ class KVMMicroHypervisor(hv_base.BaseHypervisor):
     cmd = [pathutils.KVM_CONSOLE_WRAPPER,
            constants.SOCAT_PATH, utils.ShellQuote(instance.name),
            utils.ShellQuote(cls._InstanceMonitor(instance.name)),
-           "STDIO,%s" % cls._SocatUnixConsoleParams(),
-           "UNIX-CONNECT:%s" % cls._InstanceSerial(instance.name)]
+           f"STDIO,{cls._SocatUnixConsoleParams()}",
+           f"UNIX-CONNECT:{cls._InstanceSerial(instance.name)}"]
     ndparams = node_group.FillND(primary_node)
     return objects.InstanceConsole(instance=instance.name,
                                    kind=constants.CONS_SSH,
@@ -483,5 +530,21 @@ class KVMMicroHypervisor(hv_base.BaseHypervisor):
                                    user=constants.SSH_CONSOLE_USER,
                                    command=cmd)
 
+  def Verify(self, hvparams=None):
+    """Verify the hypervisor.
 
+    @type hvparams: dict of strings
+    @param hvparams: hypervisor parameters to be verified against
 
+    @return: Problem description if something is wrong, C{None} otherwise
+
+    """
+    msgs = []
+    kvmpath = constants.HV_KVM_MICRO_PATH
+    if hvparams is not None:
+      kvmpath = hvparams.get(constants.HV_KVM_MICRO_PATH,
+                             constants.HV_KVM_MICRO_PATH)
+    if not os.path.exists(kvmpath):
+      msgs.append(f"The KVM binary ('{kvmpath}') does not exist")
+
+    return self._FormatVerifyResults(msgs)
